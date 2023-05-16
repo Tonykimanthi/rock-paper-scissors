2913a0f041f30bc88f75a436f63a5ceea61c58f4
Tony Km <kimaxhanthony9@gmail.com>
2023-05-16 22:15:24 +0300

    Added images

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ let gameImages = [
 const player = document.querySelector(".player");
 const computer = document.querySelector(".computer");
 
-const imagesToChoose = document.querySelector(".images-to-choose").children
+const imagesToChoose = document.querySelector(".images-to-choose")
 
 const rockContainer = document.querySelector(".rock");
 const paperContainer = document.querySelector(".paper");
@@ -21,6 +21,51 @@ computer.style.backgroundPosition = "top";
 computer.style.backgroundSize = "cover";
 computer.style.backgroundRepeat = "no-repeat";
 
-imagesToChoose.style.backgroundImage = `url(${gameImages[randomImage]})`
+// Player image
+player.style.backgroundPosition = "top";
+player.style.backgroundSize = "cover";
+player.style.backgroundRepeat = "no-repeat";
 
-console.log(imagesToChoose)
+// Creating image containers
+const rock = document.createElement("div")
+imagesToChoose.appendChild(rock);
+rock.style.border = "1px solid"
+rock.style.width = "10em"
+rock.style.height = "10em"
+rock.style.backgroundImage = `url(${gameImages[0]})`
+rock.style.backgroundPosition = "top";
+rock.style.backgroundSize = "cover";
+rock.style.backgroundRepeat = "no-repeat";
+rock.classList.add("clicked-image")
+
+
+const paper = document.createElement("div")
+imagesToChoose.appendChild(paper);
+paper.style.border = "1px solid"
+paper.style.width = "10em"
+paper.style.height = "10em"
+paper.style.backgroundImage = `url(${gameImages[1]})`
+paper.style.backgroundPosition = "top";
+paper.style.backgroundSize = "cover";
+paper.style.backgroundRepeat = "no-repeat";
+paper.classList.add("clicked-image")
+
+const scissor = document.createElement("div")
+imagesToChoose.appendChild(scissor);
+scissor.style.border = "1px solid"
+scissor.style.width = "10em"
+scissor.style.height = "10em"
+scissor.style.backgroundImage = `url(${gameImages[2]})`
+scissor.style.backgroundPosition = "top";
+scissor.style.backgroundSize = "cover";
+scissor.style.backgroundRepeat = "no-repeat";
+scissor.classList.add("clicked-image")
+
+console.log(rock)
+let imageChose = document.querySelectorAll(".clicked-image") 
+imageChose.forEach(function(image){
+    image.addEventListener("click", function(){
+        const source = image.style.backgroundImage
+        player.style.backgroundImage = `${source}`
+    })
+})
