3fed5a780c4d7541706dbd9b0655bb2d1cd0ad5e
Tony Km <kimaxhanthony9@gmail.com>
2023-05-17 00:44:54 +0300

    Almost done

--- public/script.js
+++ public/script.js
@@ -10,16 +10,10 @@ const computer = document.querySelector(".computer");
 
 const imagesToChoose = document.querySelector(".images-to-choose")
 
-const rockContainer = document.querySelector(".rock");
-const paperContainer = document.querySelector(".paper");
-const scissorContainer = document.querySelector(".scissor");
+const refreshBtn = document.getElementById("refresh-btn");
 
-let randomImage = Math.floor(Math.random() * gameImages.length)
+let randomImage = Math.floor(Math.random() * gameImages.length);
 
-computer.style.backgroundImage = `url(${gameImages[randomImage]})`
-computer.style.backgroundPosition = "top";
-computer.style.backgroundSize = "cover";
-computer.style.backgroundRepeat = "no-repeat";
 
 // Player image
 player.style.backgroundPosition = "top";
@@ -29,43 +23,82 @@ player.style.backgroundRepeat = "no-repeat";
 // Creating image containers
 const rock = document.createElement("div")
 imagesToChoose.appendChild(rock);
-rock.style.border = "1px solid"
 rock.style.width = "10em"
 rock.style.height = "10em"
 rock.style.backgroundImage = `url(${gameImages[0]})`
 rock.style.backgroundPosition = "top";
 rock.style.backgroundSize = "cover";
 rock.style.backgroundRepeat = "no-repeat";
+rock.style.borderRadius = "8px";
+rock.style.cursor = "pointer";
 rock.classList.add("clicked-image")
 
 
 const paper = document.createElement("div")
 imagesToChoose.appendChild(paper);
-paper.style.border = "1px solid"
 paper.style.width = "10em"
 paper.style.height = "10em"
 paper.style.backgroundImage = `url(${gameImages[1]})`
 paper.style.backgroundPosition = "top";
 paper.style.backgroundSize = "cover";
 paper.style.backgroundRepeat = "no-repeat";
+paper.style.borderRadius = "8px";
+paper.style.cursor = "pointer";
 paper.classList.add("clicked-image")
 
 const scissor = document.createElement("div")
 imagesToChoose.appendChild(scissor);
-scissor.style.border = "1px solid"
 scissor.style.width = "10em"
 scissor.style.height = "10em"
 scissor.style.backgroundImage = `url(${gameImages[2]})`
 scissor.style.backgroundPosition = "top";
 scissor.style.backgroundSize = "cover";
 scissor.style.backgroundRepeat = "no-repeat";
+scissor.style.borderRadius = "8px";
+scissor.style.cursor = "pointer";
 scissor.classList.add("clicked-image")
 
-console.log(rock)
-let imageChose = document.querySelectorAll(".clicked-image") 
+let imageChose = document.querySelectorAll(".clicked-image");
+
 imageChose.forEach(function(image){
     image.addEventListener("click", function(){
-        const source = image.style.backgroundImage
-        player.style.backgroundImage = `${source}`
+        const score = document.getElementById("score");
+
+        const playerChose = image.style.backgroundImage
+        player.style.backgroundImage = `${playerChose}`
+
+        computer.style.backgroundImage = `url(${gameImages[randomImage]})`
+        computer.style.backgroundPosition = "top";
+        computer.style.backgroundSize = "cover";
+        computer.style.backgroundRepeat = "no-repeat";
+
+        const computerChose = computer.style.backgroundImage;
+
+        if(playerChose === computerChose){
+            score.innerText = "Tie";
+        }else if(playerChose.includes(gameImages[0]) && computerChose.includes(gameImages[1])){
+            score.innerText = "You Lose";
+            console.log("You Lose");
+        }else if(playerChose.includes(gameImages[0]) && computerChose.includes(gameImages[2])){
+            score.innerText = "You Win";
+        }else if(playerChose.includes(gameImages[1]) && computerChose.includes(gameImages[0])){
+            score.innerText = "You Win";
+        }else if(playerChose.includes(gameImages[1]) && computerChose.includes(gameImages[2])){
+            score.innerText = "You Lose";
+        }else if(playerChose.includes(gameImages[2]) && computerChose.includes(gameImages[0])){
+            score.innerText = "You Lose";
+        }else if(playerChose.includes(gameImages[2]) && computerChose.includes(gameImages[1])){
+            score.innerText = "You Win";
+        }
+
+        rock.style.pointerEvents = "none";
+        paper.style.pointerEvents = "none";
+        scissor.style.pointerEvents = "none";
     })
+})
+
+
+// Refresh Page
+refreshBtn.addEventListener("click", function(){
+    location.reload()
 })
